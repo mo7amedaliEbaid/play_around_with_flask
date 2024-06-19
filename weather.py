98d295a903f6e1209c0948be0195201024609ba2
Mohamed Ali <mo7amedaliebaid@gmail.com>
2024-06-19 23:41:33 +0300

    weather

---
+++ weather.py
@@ -0,0 +1,23 @@
+from dotenv import load_dotenv
+from pprint import pprint
+import requests
+import os
+
+
+load_dotenv()
+
+def get_current_weather(city="Cairo City"):
+    request_url = f'http://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=imperial'
+    weather_data=requests.get(request_url).json()
+    return weather_data
+
+if __name__== "__main__":
+    print('\n*** Get Current Weather Conditions ***\n')
+
+    city=input("\nPlease enter a city name: ")
+
+    weather_data=get_current_weather(city)
+
+    print("\n")
+
+    pprint(weather_data)
